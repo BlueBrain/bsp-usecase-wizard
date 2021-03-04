2ab961f3a1133d6f2dd6c04f8c4a585ad988f555
Stefano Antonel <stefano.antonel@epfl.ch>
2021-03-04 11:42:05 +0100

    Change ModelShowSelectedToggle for a list

--- src/components/ModelShowSelectedToggle.svelte
+++ src/components/ModelShowSelectedToggle.svelte
@@ -1,54 +1,34 @@
 
 <script lang="ts">
   import { modelsSelected } from '@/store';
-  let showModelsSelected: boolean = true;
 </script>
 
 
 
-<div class="container">
-  <div
-    class="
-      models-selected-container
-      {showModelsSelected ? 'expanded' : 'collapsed'}
-      {$modelsSelected.length ? 'visible' : 'hide-toggle'}
-    "
-  >
-    <div class="models-accordion" on:click={() => showModelsSelected = !showModelsSelected}>
-      <div>{ showModelsSelected ? 'Hide' : 'Show' } Models Selected</div>
-    </div>
-    {#each $modelsSelected as modelItem}
-    <p>{ modelItem.name }</p>
-    {/each}
-  </div>
+<div class="models-selected-container {$modelsSelected.length ? '' : 'hidden-section'}">
+  <span class="title">Models selected ({$modelsSelected.length}):</span>
+  {#each $modelsSelected as modelItem}
+    <span class="model">{ modelItem.name }</span>
+  {/each}
 </div>
 
 
 
 <style>
   .models-selected-container {
-    margin: 10px 0;
-    cursor: pointer;
-    border: 1px solid;
-    border-radius: 5px;
-    padding: 15px;
+    padding: 15px 10px;
   }
-  .models-selected-container .models-accordion {
-    text-align: center;
-    font-size: 30px;
-    border-radius: 5px;
+  .models-selected-container span.title {
+    font-size: 16px;
   }
-  .models-selected-container.expanded {
-    max-height: 400px;
-    transition: max-height 0.3s ease-out;
+  .models-selected-container span.model {
+    font-size: 12px;
   }
-  .models-selected-container.collapsed {
-    max-height: 35px;
-    transition: max-height 0.3s ease-out;
-    height:auto;
-    overflow: hidden;
+  .models-selected-container span.model::after {
+    content: ", ";
   }
-  .models-selected-container.hide-toggle {
+  
+  .models-selected-container.hidden-section {
     display: none;
   }
 </style>
