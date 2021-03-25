734f5468369353851eab558f3e92fcc4a8c3468d
Stefano Antonel <stefano.antonel@epfl.ch>
2021-03-25 09:04:38 +0100

    Testing Drive (TODO api needs auth)

--- tests/ebrains-drive.test.js
+++ tests/ebrains-drive.test.js
@@ -1,5 +1,9 @@
-const { drive } = require('../src/constants');
+
+import { drive } from '@/constants';
 
 test('api url is correct', () => {
   expect(drive.DRIVE_API_URL).toBe('https://drive.ebrains.eu/api2');
 });
+
+
+// TODO: test the API with authentication
