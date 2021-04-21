9f09fbe0e8aa17e8d473702b80223fb82a9f5006
Stefano Antonel <stefano.antonel@epfl.ch>
2021-04-21 16:20:38 +0200

    Add CLS-INTERACTIVE-UC/repo_name structure

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -40,18 +40,29 @@ export function justCheckIfAuthenticated() {
   });
 }
 
+function getRepoName(repoUrl: string) {
+  // if has branch take the first part only
+  const url = repoUrl.includes('&') ? repoUrl.split('&')[0] : repoUrl;
+  const name = url.split('/').pop();
+  const result = decodeURIComponent(name);
+  return result;
+}
+
 export function openPuller(collabName: string) {
   // TODO: fix assigning type based on property
   const uc = get(usecaseSelected);
 
   // @ts-ignore
-  const repo = `${nbgitpuller.REPO_KEY}${uc.notebookRepoUrl}`;
-  const collabPath = collabName === 'My Library' ? 'drive/My Libraries/My Library' : `shared/${collabName}`;
-  const targetPath = `${nbgitpuller.TARGET_PATH_KEY}/${collabPath}/${drive.DEFAULT_UC_FOLDER_NAME}`;
+  const repoName = getRepoName(uc.notebookRepoUrl);
   // @ts-ignore
-  const urlPath = `${nbgitpuller.URL_PATH_KEY}/${collabPath}/${drive.DEFAULT_UC_FOLDER_NAME}/${uc.notebookPath}`;
+  const repoUrl = `${nbgitpuller.REPO_KEY}${uc.notebookRepoUrl}`;
+  const collabPath = collabName === 'My Library' ? 'drive/My Libraries/My Library' : `shared/${collabName}`;
+  const filePath = `${collabPath}/${drive.DEFAULT_UC_FOLDER_NAME}/${repoName}`;
+  const targetPath = `${nbgitpuller.TARGET_PATH_KEY}/${filePath}`;
+  // @ts-ignore
+  const urlPath = `${nbgitpuller.URL_PATH_KEY}/${filePath}/${uc.notebookPath}`;
 
-  const pullerLink = `${nbgitpuller.BASE}${repo}${targetPath}${urlPath}`;
+  const pullerLink = `${nbgitpuller.BASE}${repoUrl}${targetPath}${urlPath}`;
   console.debug('PullerLink:', pullerLink);
   window.open(pullerLink, '_blank');
   return pullerLink;
