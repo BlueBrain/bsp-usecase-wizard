932c158a38d84ee34b62880e0576b1fb1e31e1a4
Stefano Antonel <stefano.antonel@epfl.ch>
2021-04-29 11:05:27 +0200

    Style update Use Case selection Accordion

--- src/components/Accordion.svelte
+++ src/components/Accordion.svelte
@@ -23,51 +23,46 @@
 
 
 
-<style type="text/scss">
-  $midnight: #2c3e50;
-  $clouds: #ecf0f1;
-
+<style>
   .tab {
-    width: 100%;
-    color: white;
-    overflow: hidden;
-    &-label {
-      display: flex;
-      justify-content: space-between;
-      padding: 1em;
-      background: $midnight;
-      font-weight: bold;
-      cursor: pointer;
-      /* Icon */
-      &:hover {
-        background: darken($midnight, 10%);
-      }
-      &::after {
-        content: "\276F";
-        width: 1em;
-        height: 1em;
-        text-align: center;
-        transition: all .35s;
-      }
-    }
-    &-content {
-      max-height: 0;
-      padding: 0 1em;
-      color: $midnight;
-      background: white;
-      transition: all .35s;
-    }
-    
-    .tab-content.expanded {
-      max-height: 500vh;
-      padding: 1em;
-    }
-
-    .tab-label.expanded {
-      background: darken($midnight, 10%);
-      &::after {
-        transform: rotate(90deg);
-      }
-    }
+	 width: 100%;
+	 color: white;
+	 overflow: hidden;
+  }
+  .tab-label {
+    display: flex;
+    justify-content: space-between;
+    padding: 1em;
+    background: #2c3e50;
+    font-weight: bold;
+    cursor: pointer;
+    /* Icon */
+  }
+  .tab-label:hover {
+    background: #1a252f;
+  }
+  .tab-label::after {
+    content: "\276F";
+    width: 1em;
+    height: 1em;
+    text-align: center;
+    transition: all 0.35s;
+  }
+  .tab-content {
+    max-height: 0;
+    padding: 0 1em;
+    color: #2c3e50;
+    background: white;
+    transition: all 0.35s;
+  }
+  .tab .tab-content.expanded {
+    max-height: 500vh;
+    padding: 1em;
+  }
+  .tab .tab-label.expanded {
+    background: #1a252f;
+  }
+  .tab .tab-label.expanded::after {
+    transform: rotate(90deg);
   }
 </style>
