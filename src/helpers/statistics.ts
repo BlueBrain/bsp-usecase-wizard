269477e732bfeb44883b76e50e9d4dab1dd215b8
Stefano Antonel <stefano.antonel@epfl.ch>
2021-05-04 09:55:41 +0200

    Add IP to statistics

--- src/helpers/statistics.ts
+++ src/helpers/statistics.ts
@@ -11,6 +11,7 @@ import {
 import { usecases } from '@/constants';
 
 const STATISTICS_FORM_URL = usecases.STATISTIC_URL;
+const IP_ENDPOINT = 'https://checkip.amazonaws.com';
 
 export interface StatisticDataInterface {
   category: string;
@@ -18,16 +19,25 @@ export interface StatisticDataInterface {
   userId: string;
   models?: Array<string>;
   collabId?: string;
+  ip: string;
 }
 
-export function sendStatistics() {
+async function getAnonymizedIp() {
+  const ip = (await axios.get(IP_ENDPOINT)).data || '';
+  const parts = ip.split('.');
+  // remove the last identifier
+  parts.splice(-1, 1, "0");
+  return parts.join('.');
+}
+
+export async function sendStatistics() {
   // avoid sending outside production or in test
   // @ts-ignore // is testing
   if (typeof jest !== 'undefined') return;
   // @ts-ignore // comes from rollup.config.js
   declare const processEnvs: any;
   if (processEnvs?.baseUrl !== '/static/wizard') return;
-  
+
   const formData = new URLSearchParams();
   const data: StatisticDataInterface = {
     category: get(usecaseCategorySelected),
@@ -35,6 +45,7 @@ export function sendStatistics() {
     userId: get(userInfo)?.profile?.sub,
     models: get(modelsSelected)?.map(m => m.id),
     collabId: get(collabIdSelected),
+    ip: await getAnonymizedIp(),
   }
   
   formData.append('entry.2065854000', data.category);
@@ -42,6 +53,7 @@ export function sendStatistics() {
   formData.append('entry.2088231351', data.models.toString());
   formData.append('entry.748800890', data.collabId);
   formData.append('entry.1933333390', data.userId);
+  formData.append('entry.1813902573', data.ip);
 
   const send = () => {
     axios({
